ea622068ebb1f538bf77ce44c900736400e3cf27
Izushi <shoeshinesamurai@gmail.com>
2024-09-24 12:20:26 -0700

    ex01 is done

--- ex01.js
+++ ex01.js
@@ -8,6 +8,17 @@ Create a function named sumLargestNumbers that will receive an array of numbers 
 
 const sumLargestNumbers = function (data) {
   // Put your solution here
+  let largest = 0;
+  let secondLargest = 0;
+  data.forEach(num => {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest) {
+      secondLargest = num;
+    }
+  });
+  return largest + secondLargest;
 };
 
 console.log(sumLargestNumbers([1, 10])); // 11
