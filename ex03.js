a4cea09f76b99494ebaa1f01e49cb1e1bb2f0c96
Izushi <shoeshinesamurai@gmail.com>
2024-09-25 08:46:15 -0700

    ex03 is done

--- ex03.js
+++ ex03.js
@@ -9,6 +9,14 @@ Create a function named numberOfVowels that will receive a string and return the
 
 const numberOfVowels = function (data) {
   // Put your solution here
+  let vowelsCounter = 0;
+  let lettersArr = data.split('');
+  lettersArr.filter(letter => {
+    if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+      vowelsCounter++;
+    }
+  })
+  return vowelsCounter;
 };
 
 console.log(numberOfVowels("orange")); // 3
